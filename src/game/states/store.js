951dbe25fa587704c8e91e790d57b70a8a939337
Victor Hugo Batista <vct.bta@gmail.com>
2025-10-06 16:42:16 -0300

    Add initial game state

--- src/game/states/store.js
+++ src/game/states/store.js
@@ -3,23 +3,36 @@ import {create} from "zustand";
 import { constants } from "./constants";
 
 export const useGameStore = create(set => ({
-  gameArea: [{
-    x: 10,
-    y: 10,
-    size: constants.stepSizePercent,
-  },
-  {
-    x: 50,
-    y: 50,
-    size: constants.stepSizePercent,
-  },
-  {
-    x: 60,
-    y: 80,
-    size: constants.stepSizePercent,
-  }],
+  gameArea: [
+    {
+      x: 50,
+      y: 50,
+      size: constants.stepSizePercent,
+    },
+    {
+      x: 50 - constants.stepSizePercent,
+      y: 50,
+      size: constants.stepSizePercent,
+    },
+    {
+      x: 50 - constants.stepSizePercent * 2,
+      y: 50,
+      size: constants.stepSizePercent,
+    },
+    {
+      x: 50 - constants.stepSizePercent * 3,
+      y: 50,
+      size: constants.stepSizePercent,
+    },
+    {
+      x: 50 - constants.stepSizePercent * 4,
+      y: 50,
+      size: constants.stepSizePercent,
+    },
+  ],
   addItem: item => (set((state) => {
     const gameAreaToApdate = state.gameArea.slice(0);
+    item.size = constants.stepSizePercent;
     gameAreaToApdate.push(({gameArea: item}));
     return {gameArea: gameAreaToApdate};
   })),
