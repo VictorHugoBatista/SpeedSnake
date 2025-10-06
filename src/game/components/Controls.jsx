44bf59f568419d3fd201cd6d9b8ea060dab9a373
Victor Hugo Batista <vct.bta@gmail.com>
2025-10-06 14:00:54 -0300

    Show controls on mobile version or on button click

--- src/game/components/Controls.jsx
+++ src/game/components/Controls.jsx
@@ -1,20 +1,40 @@
+import { useEffect, useState } from "react";
+
+import useScreenSize from "../../utils/screenSize";
+
 const Controls = function () {
+  const screenSize = useScreenSize();
+
+  const [showControls, setShowControls] = useState(false);
+
+  useEffect(() => {
+    if (screenSize.width < 768) {
+      setShowControls(true);
+    }
+  }, [screenSize]);
+
+  const toggleControls = () => {
+    setShowControls((showControls) => ! showControls);
+  };
+
   return (
     <>
-      <div class="button-virtual-buttons">
+      <div className="button-virtual-buttons">
         <span>Press P to Pause</span>
-        <div className="button button-blue">Virtual controls</div>
+        <div className="button button-blue" onClick={toggleControls}>Virtual controls</div>
       </div>
-      <div className="controls">
-        <div className="controls-up">
-          <div className="button controls-button">up</div>
+      {showControls ?
+        <div className="controls">
+          <div className="controls-up">
+            <div className="button controls-button">up</div>
+          </div>
+          <div className="controls-down">
+            <div className="button controls-button">left</div>
+            <div className="button controls-button">down</div>
+            <div className="button controls-button">right</div>
+          </div>
         </div>
-        <div className="controls-down">
-          <div className="button controls-button">left</div>
-          <div className="button controls-button">down</div>
-          <div className="button controls-button">right</div>
-        </div>
-      </div>
+      : null}
     </>
   );
 };
