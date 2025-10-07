a861d1a0451e479c006a9d76b64087a5f218ed6f
Victor Hugo Batista <vct.bta@gmail.com>
2025-10-07 16:58:33 -0300

    Solve footer link linter issue

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="footer">
-      2015 - {new Date().getFullYear()} Remade with love by <a target="_blank" href="https://github.com/VictorHugoBatista">@VictorHugoBatista</a>
+      2015 - {new Date().getFullYear()} Remade with love by <a target="_blank" rel="noreferrer" href="https://github.com/VictorHugoBatista">@VictorHugoBatista</a>
     </footer>
   );
 }
