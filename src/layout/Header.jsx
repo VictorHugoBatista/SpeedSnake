2eb04bda1d8323a7d6f6ebacbfecc6dd5617646b
Victor Hugo Batista <vct.bta@gmail.com>
2025-10-07 22:56:50 -0300

    Improve header mobile

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,16 +3,12 @@ import { Link } from 'react-router';
 function Header() {
   return (
     <header className="header">
+      <span className="header-title"><Link to="/">SpeedSnake 2025</Link></span>
       <nav>
         <ul>
           <li>
             <Link to="/about">About</Link>
           </li>
-        </ul>
-      </nav>
-      <span className="header-title"><Link to="/">SpeedSnake 2025</Link></span>
-      <nav>
-        <ul>
           <li>
             <a target="_blank" href="legacy">Legacy</a>
           </li>
